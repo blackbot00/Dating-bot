0ede2897c457a3ea017c4a09012a0a87e21b42c1
blackbot00 <109160013+blackbot00@users.noreply.github.com>
2026-01-20 20:49:08 +0530

    Create Constants.py

---
+++ app/Constants.py
@@ -0,0 +1,41 @@
+STATES = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal"
+]
+
+GENDERS = ["Male", "Female", "Transgender"]
+
+LANGUAGES = ["Tamil", "English", "Hindi", "Telugu"]
+
+AI_STYLES = ["Sweet", "Romantic", "Caring", "Possessive", "Flirty"]
+
+MIN_AGE = 18
+MAX_AGE = 80
+
+AGE_DIFF_LIMIT = 8
